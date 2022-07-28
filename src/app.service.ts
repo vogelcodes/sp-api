82411df8c16152761243c8f9c6983da7b5a73eb7
Daniel <danielcvt@gmail.com>
2022-07-28 19:27:02 -0300

    credits

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Siggia&Procaci - API';
+    return 'Siggia&Procaci - API \n Desenvolvido Por VogelCodes';
   }
 }
